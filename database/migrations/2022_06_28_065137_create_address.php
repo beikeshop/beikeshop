fb1a2a16f19f0b7a26cc4d09ab85d561cb60a7d5
Edward Yang <yangjin@opencart.cn>
2022-06-28 15:21:32 +0800

    add addresses

---
+++ database/migrations/2022_06_28_065137_create_address.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddress extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('customer_id');
+            $table->string('name');
+            $table->string('phone');
+            $table->unsignedInteger('country_id');
+            $table->unsignedInteger('state_id');
+            $table->string('state');
+            $table->unsignedInteger('city_id');
+            $table->string('city');
+            $table->string('zipcode');
+            $table->string('address_1');
+            $table->string('address_2');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+}
