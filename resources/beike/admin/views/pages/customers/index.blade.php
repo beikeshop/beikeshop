5358f7f1838e1f2e75db15377449e7b0508377ab
pushuo <229102104@qq.com>
2022-07-01 17:54:52 +0800

    wip

--- resources/beike/admin/views/pages/customers/index.blade.php
+++ resources/beike/admin/views/pages/customers/index.blade.php
@@ -41,8 +41,23 @@
           </tr>
         </tbody>
       </table>
-    </div>
 
+      <nav aria-label="...">
+        <ul class="pagination">
+          <li class="page-item disabled">
+            <a class="page-link">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active" aria-current="page">
+            <a class="page-link" href="#">2</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
     <el-dialog title="创建顾客" :visible.sync="dialogCustomers.show" width="600px"
       @close="closeCustomersDialog('form')" :close-on-click-modal="false">
       <el-form ref="form" :rules="rules" :model="dialogCustomers.form" label-width="100px">
