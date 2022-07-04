19c4e9291c3efdebf949ea96ef9adc8d6f3d266f
pushuo <229102104@qq.com>
2022-07-04 14:10:13 +0800

    wip

--- resources/beike/admin/views/pages/login/login.blade.php
+++ resources/beike/admin/views/pages/login/login.blade.php
@@ -11,45 +11,42 @@
 <body class="page-login">
   <div class="d-flex align-items-center vh-100 pt-2 pt-sm-5 pb-4 pb-sm-5">
     <div class="container">
-      <div class="justify-content-center row">
-        <div class="col-xl-5 col-lg-5 col-md-6 mx-auto">
-          <div class="card">
-            <div class="w-480">
-              <div class="card-header mt-3 mb-4">
-                <h4 class="fw-bold">登录到 beikeshop 后台</h4>
-                <div class="text-muted fw-normal">登录到 beikeshop 后台</div>
+      <div class="card">
+        <div class="w-480">
+          <div class="card-header mt-3 mb-4">
+            <h4 class="fw-bold">登录到 beikeshop 后台</h4>
+            <div class="text-muted fw-normal">登录到 beikeshop 后台</div>
+          </div>
+
+          <div class="card-body">
+            <form action="{{ admin_route('login.store') }}" method="post">
+              @csrf
+
+              <div class="form-floating mb-4">
+                <input type="text" name="email" class="form-control" id="email-input" value="{{ old('email') }}" placeholder="邮箱地址">
+                <label for="email-input">邮箱地址</label>
+                @error('email')
+                  <x-admin::form.error :message="$message" />
+                @enderror
               </div>
 
-              <div class="card-body">
-                <form action="{{ admin_route('login.store') }}" method="post">
-                  @csrf
-
-                  <div class="form-floating mb-4">
-                    <input type="text" name="email" class="form-control" id="email-input" value="{{ old('email') }}" placeholder="邮箱地址">
-                    <label for="email-input">邮箱地址</label>
-                    @error('email')
-                      <x-admin::form.error :message="$message" />
-                    @enderror
-                  </div>
-
-                  <div class="form-floating mb-5">
-                    <input type="password" name="password" class="form-control" id="password-input" placeholder="密码">
-                    <label for="password-input">密码</label>
-                    @error('password')
-                      <x-admin::form.error :message="$message" />
-                    @enderror
-                  </div>
-
-                  @if (session('error'))
-                    <div class="alert alert-success">
-                      {{ session('error') }}
-                    </div>
-                  @endif
-
-                  <div class="d-grid mb-4"><button type="submit" class="btn btn-lg btn-primary">登录</button></div>
-                </form>
+              <div class="form-floating mb-5">
+                <input type="password" name="password" class="form-control" id="password-input" placeholder="密码">
+                <label for="password-input">密码</label>
+                @error('password')
+                  <x-admin::form.error :message="$message" />
+                @enderror
               </div>
-            </div>
+
+              @if (session('error'))
+                <div class="alert alert-success">
+                  {{ session('error') }}
+                </div>
+              @endif
+
+              <div class="d-grid mb-4"><button type="submit" class="btn btn-lg btn-primary">登录</button></div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
