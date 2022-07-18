ce79996249d03de095bd14d5e81525a7fb6396c0
Edward Yang <yangjin@opencart.cn>
2022-07-18 12:03:01 +0800

    fixed black

--- themes/black/home.blade.php
+++ themes/black/home.blade.php
@@ -6,7 +6,7 @@
     <section class="module-image-plus mb-5">
         <div class="container">
             <div class="row">
-                这里是 black 模板, 系统优先读取, 删除该文件即可调用 default 模板
+                这里是 black 模板, 后台设置为默认模板后系统优先读取, 删除该文件系统将调用 default 模板下对应文件
             </div>
         </div>
     </section>
