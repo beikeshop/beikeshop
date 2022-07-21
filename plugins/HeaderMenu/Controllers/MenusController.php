607f9ee28096d2efe6b365839a4bc1b92908ac20
Edward Yang <yangjin@opencart.cn>
2022-07-21 10:59:19 +0800

    按最后修改排序

--- plugins/HeaderMenu/Controllers/MenusController.php
+++ plugins/HeaderMenu/Controllers/MenusController.php
@@ -18,7 +18,7 @@ class MenusController extends Controller
 {
     public function latestProducts()
     {
-        $products = ProductRepo::getBuilder()->orderByDesc('created_at')->paginate(40);
+        $products = ProductRepo::getBuilder()->orderByDesc('updated_at')->paginate(40);
         return view("HeaderMenu::latest_products", ['products' => $products]);
     }
 }
