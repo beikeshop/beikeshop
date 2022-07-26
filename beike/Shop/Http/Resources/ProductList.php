b8c21f66e5940d606f329f453ba61857d62d1935
Edward Yang <yangjin@opencart.cn>
2022-07-26 15:27:55 +0800

    add origin price.

--- beike/Shop/Http/Resources/ProductList.php
+++ beike/Shop/Http/Resources/ProductList.php
@@ -11,10 +11,18 @@
 
 namespace Beike\Shop\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductList extends JsonResource
 {
+    /**
+     * 图片列表页Item
+     *
+     * @param Request $request
+     * @return array
+     * @throws \Exception
+     */
     public function toArray($request): array
     {
         return [
@@ -22,6 +30,7 @@ class ProductList extends JsonResource
             'name' => $this->description->name ?? '',
             'url' => shop_route('products.show', ['product' => $this]),
             'price' => $this->price,
+            'origin_price' => $this->origin_price,
             'image' => image_resize($this->image),
             'price_format' => currency_format($this->price),
             'category_id' => $this->category_id ?? null,
