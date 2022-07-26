3bcece155edd7a633aafc5ef406ecb55b58243ed
Edward Yang <yangjin@opencart.cn>
2022-07-26 09:18:43 +0800

    update home template

--- themes/black/home.blade.php
+++ themes/black/home.blade.php
@@ -6,7 +6,7 @@
     <section class="module-image-plus mb-5">
         <div class="container">
             <div class="row">
-                这里是 black 模板, 后台设置为默认模板后系统优先读取, 删除该文件系统将调用 default 模板下对应文件
+                这里是 black 模板, 后台设置使用该模板后系统优先读取该模板, 删除该文件系统将调用 default 模板下对应文件
             </div>
         </div>
     </section>
