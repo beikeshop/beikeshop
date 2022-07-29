0be99e30fb072556ef7d246f84aa1f2c30b5469b
Edward Yang <yangjin@opencart.cn>
2022-07-29 18:40:41 +0800

    fixed payment address id

--- beike/Repositories/CartRepo.php
+++ beike/Repositories/CartRepo.php
@@ -24,17 +24,27 @@ class CartRepo
         }
         $customerId = $customer->id;
         $cart = Cart::query()->where('customer_id', $customerId)->first();
+        $defaultAddress = AddressRepo::listByCustomer($customer)->first();
+        $defaultAddressId = $defaultAddress->id ?? 0;
+
         if (empty($cart)) {
             $shippingMethod = PluginRepo::getShippingMethods()->first();
             $paymentMethod = PluginRepo::getPaymentMethods()->first();
-            $defaultAddress = AddressRepo::listByCustomer($customer)->first();
             $cart = Cart::query()->create([
                 'customer_id' => $customerId,
-                'shipping_address_id' => $defaultAddress->id ?? 0,
+                'shipping_address_id' => $defaultAddressId,
                 'shipping_method_code' => $shippingMethod->code,
-                'payment_address_id' => $defaultAddress->id ?? 0,
+                'payment_address_id' => $defaultAddressId,
                 'payment_method_code' => $paymentMethod->code
             ]);
+        } else {
+            if ($cart->shipping_address_id == 0) {
+                $cart->shipping_address_id = $defaultAddressId;
+            }
+            if ($cart->payment_address_id == 0) {
+                $cart->payment_address_id = $defaultAddressId;
+            }
+            $cart->save();
         }
         return $cart;
     }
