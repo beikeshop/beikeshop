af2302ccf5633c632fcf6761bc24ad55b017b08d
pushuo <229102104@qq.com>
2022-08-03 16:31:36 +0800

    订单状态

--- resources/beike/admin/views/pages/orders/form.blade.php
+++ resources/beike/admin/views/pages/orders/form.blade.php
@@ -50,6 +50,39 @@
       </div>
     </div>
   </div>
+
+  <div class="card mb-4">
+    <div class="card-header"><h6 class="card-title">状态</h6></div>
+    <div class="card-body" id="app">
+      <el-form ref="form" :model="form" :rules="rules" label-width="100px">
+        <el-form-item label="当前状态">
+          待支付
+        </el-form-item>
+        <el-form-item label="修改状态" prop="status">
+          <el-select size="small" v-model="form.status" placeholder="请选择">
+            <el-option
+              v-for="item in statuses"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="通知客户">
+          {{-- <el-checkbox v-model="form.notify"></el-checkbox> --}}
+          <el-switch v-model="form.notify">
+          </el-switch>
+        </el-form-item>
+        <el-form-item label="备注信息">
+          <textarea class="form-control w-max-500" v-model="form.comment"></textarea>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm('form')">更新状态</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+
   <div class="card mb-4">
     <div class="card-header"><h6 class="card-title">商品信息</h6></div>
     <div class="card-body">
@@ -99,4 +132,50 @@
   </div>
 @endsection
 
+@push('footer')
+  <script>
+    new Vue({
+      el: '#app',
+
+      data: {
+        statuses: [{"value":"pending","label":"待处理"},{"value":"rejected","label":"已拒绝"},{"value":"approved","label":"已批准（待顾客寄回商品）"},{"value":"shipped","label":"已发货（寄回商品）"},{"value":"completed","label":"已完成"}],
+        form: {
+          status: "",
+          notify: false,
+          comment: '',
+        },
+
+        rules: {
+          status: [{required: true, message: '请输入用户名', trigger: 'blur'}, ],
+        }
+      },
+
+      beforeMount() {
+        // let statuses = @json($statuses ?? []);
+        // this.statuses = Object.keys(statuses).map(key => {
+        //   return {
+        //     value: key,
+        //     label: statuses[key]
+        //   }
+        // });
+      },
+
+      methods: {
+        submitForm(form) {
+          this.$refs[form].validate((valid) => {
+            if (!valid) {
+              layer.msg('请检查表单是否填写正确',()=>{});
+              return;
+            }
+
+            $http.put(`/orders/{{ $order->id }}/status`,this.form).then((res) => {
+              console.log(res)
+              layer.msg(res.message);
+            })
+          });
+        }
+      }
+    })
+  </script>
+@endpush
 
