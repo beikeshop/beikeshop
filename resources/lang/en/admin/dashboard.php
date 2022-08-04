bcbd5db00f9b274ac0bbcd40606dffdb1bbaf158
pushuo <229102104@qq.com>
2022-08-04 15:14:28 +0800

    wip

---
+++ resources/lang/en/admin/dashboard.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * order.php
+ *
+ * @copyright  2022 opencart.cn - All Rights Reserved
+ * @link       http://www.guangdawangluo.com
+ * @author     Edward Yang <yangjin@opencart.cn>
+ * @created    2022-08-02 14:22:41
+ * @modified   2022-08-02 14:22:41
+ */
+
+return [
+    'customer_view' => 'Customer View',
+    'order_total' => 'Order Total',
+    'customer_new' => 'Customer New',
+    'order_amount' => 'Order Smount',
+    'yesterday' => 'Yesterday',
+    'day_before' => 'A day earlier',
+];
---
+++ resources/lang/zh_cn/admin/dashboard.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * order.php
+ *
+ * @copyright  2022 opencart.cn - All Rights Reserved
+ * @link       http://www.guangdawangluo.com
+ * @author     Edward Yang <yangjin@opencart.cn>
+ * @created    2022-08-02 14:22:41
+ * @modified   2022-08-02 14:22:41
+ */
+
+return [
+    'customer_view' => '用户访问量',
+    'order_total' => '订单量',
+    'customer_new' => '新增用户',
+    'order_amount' => '销售额',
+    'yesterday' => '昨日',
+    'day_before' => '较前一日',
+];
