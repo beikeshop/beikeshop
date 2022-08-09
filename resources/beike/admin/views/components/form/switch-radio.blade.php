8ebe6a0979ad42cb624756f11d3d14494b5e3300
pushuo <229102104@qq.com>
2022-08-09 14:55:14 +0800

    wip

--- resources/beike/admin/views/components/form/switch-radio.blade.php
+++ resources/beike/admin/views/components/form/switch-radio.blade.php
@@ -1,11 +1,11 @@
 <x-admin::form.row :title="$title">
   <div class="mb-1 mt-2">
     <div class="form-check form-check-inline">
-      <input class="form-check-input" type="radio" name="{{ $name }}" id="{{ $name }}-1" value="1" {{ $value ? 'checked' : '' }}>
+      <input class="form-check-input" id="{{ $name }}-1" type="radio" name="{{ $name }}" id="{{ $name }}-1" value="1" {{ $value ? 'checked' : '' }}>
       <label class="form-check-label" for="{{ $name }}-1">启用</label>
     </div>
     <div class="form-check form-check-inline">
-      <input class="form-check-input" type="radio" name="{{ $name }}" id="{{ $name }}-0" value="0" {{ !$value ? 'checked' : '' }}>
+      <input class="form-check-input" id="{{ $name }}-0" type="radio" name="{{ $name }}" id="{{ $name }}-0" value="0" {{ !$value ? 'checked' : '' }}>
       <label class="form-check-label" for="{{ $name }}-0">禁用</label>
     </div>
   </div>
