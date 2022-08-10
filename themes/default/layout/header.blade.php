7310e822c5272e0a8d4a2d1917b658a9ea5d32a1
pushuo <229102104@qq.com>
2022-08-10 14:06:21 +0800

    头部显示用户名称错误

--- themes/default/layout/header.blade.php
+++ themes/default/layout/header.blade.php
@@ -14,7 +14,9 @@
 
           <div class="dropdown-menu" aria-labelledby="currency-dropdown">
             @foreach (currencies() as $currency)
-            <a class="dropdown-item" href="{{ shop_route('currency.switch', [$currency->code]) }}">{{ $currency->symbol_left }} {{ $currency->name }}</a>
+              <a class="dropdown-item"
+                href="{{ shop_route('currency.switch', [$currency->code]) }}">{{ $currency->symbol_left }}
+                {{ $currency->name }}</a>
             @endforeach
           </div>
         </div>
@@ -26,7 +28,8 @@
 
           <div class="dropdown-menu" aria-labelledby="language-dropdown">
             @foreach ($languages as $language)
-            <a class="dropdown-item" href="{{ shop_route('lang.switch', [$language->code]) }}">{{ $language->name }}</a>
+              <a class="dropdown-item"
+                href="{{ shop_route('lang.switch', [$language->code]) }}">{{ $language->name }}</a>
             @endforeach
           </div>
         </div>
@@ -41,52 +44,67 @@
 
   <div class="header-content py-3">
     <div class="container navbar-expand-lg">
-      <div class="logo"><a href="{{ shop_route('home.index') }}"><img src="{{ asset('image/logo.png') }}" class="img-fluid"></a></div>
+      <div class="logo"><a href="{{ shop_route('home.index') }}"><img src="{{ asset('image/logo.png') }}"
+            class="img-fluid"></a></div>
       <div class="menu-wrap">
         <ul class="navbar-nav mx-auto">
           @foreach ($categories as $category)
-          <li class="dropdown">
-            <a
-              target="{{ (isset($category['new_window']) and $category['new_window']) ? '_blank' : '_self' }}"
-              class="nav-link {{ (isset($category['children']) and $category['children']) ? 'dropdown-toggle' : '' }}"
-              href="{{ $category['url'] }}">
-              {{ $category['name'] }}
-            </a>
-            @if (isset($category['children']) and $category['children'])
-            <ul class="dropdown-menu">
-              @forelse ($category['children'] as $child)
-                <li><a target="{{ (isset($child['new_window']) and $child['new_window']) ? '_blank' : '_self' }}" href="{{ $child['url'] }}" class="dropdown-item">{{ $child['name'] }}</a></li>
-              @endforeach
-            </ul>
-            @endif
-          </li>
+            <li class="dropdown">
+              <a target="{{ (isset($category['new_window']) and $category['new_window']) ? '_blank' : '_self' }}"
+                class="nav-link {{ (isset($category['children']) and $category['children']) ? 'dropdown-toggle' : '' }}"
+                href="{{ $category['url'] }}">
+                {{ $category['name'] }}
+              </a>
+              @if (isset($category['children']) and $category['children'])
+                <ul class="dropdown-menu">
+                  @forelse ($category['children'] as $child)
+                    <li><a target="{{ (isset($child['new_window']) and $child['new_window']) ? '_blank' : '_self' }}"
+                        href="{{ $child['url'] }}" class="dropdown-item">{{ $child['name'] }}</a></li>
+                  @endforeach
+                </ul>
+              @endif
+            </li>
           @endforeach
         </ul>
 
-          {{-- <a href="{{ shop_route('categories.show', $category) }}">{{ $category->description->name }}</a> --}}
+        {{-- <a href="{{ shop_route('categories.show', $category) }}">{{ $category->description->name }}</a> --}}
       </div>
       <div class="right-btn">
         <ul class="navbar-nav flex-row">
-          <li class="nav-item"><a href="#offcanvas-search-top" data-bs-toggle="offcanvas" aria-controls="offcanvasExample" class="nav-link"><i class="iconfont">&#xe8d6;</i></a></li>
-          <li class="nav-item"><a href="{{ shop_route('account.wishlist.index') }}" class="nav-link"><i class="iconfont">&#xe662;</i></a></li>
+          <li class="nav-item"><a href="#offcanvas-search-top" data-bs-toggle="offcanvas"
+              aria-controls="offcanvasExample" class="nav-link"><i class="iconfont">&#xe8d6;</i></a></li>
+          <li class="nav-item"><a href="{{ shop_route('account.wishlist.index') }}" class="nav-link"><i
+                class="iconfont">&#xe662;</i></a></li>
           <li class="nav-item dropdown">
             <a href="{{ shop_route('account.index') }}" class="nav-link"><i class="iconfont">&#xe619;</i></a>
             <ul class="dropdown-menu dropdown-menu-end">
               @auth('web_shop')
-                <li class="dropdown-item"><h6 class="mb-0">Pu Shuo</h6></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a href="{{ shop_route('account.index') }}" class="dropdown-item"><i class="bi bi-person me-1"></i> 个人中心</a></li>
-                <li><a href="{{ shop_route('account.order.index') }}" class="dropdown-item"><i class="bi bi-clipboard-check me-1"></i> 我的订单</a></li>
-                <li><a href="{{ shop_route('account.wishlist.index') }}" class="dropdown-item"><i class="bi bi-heart me-1"></i> 我的收藏</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a href="{{ shop_route('logout') }}" class="dropdown-item"><i class="bi bi-box-arrow-left me-1"></i> 退出登录</a></li>
+                <li class="dropdown-item">
+                  <h6 class="mb-0">{{ current_customer()->name }}</h6>
+                </li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a href="{{ shop_route('account.index') }}" class="dropdown-item"><i class="bi bi-person me-1"></i>
+                    个人中心</a></li>
+                <li><a href="{{ shop_route('account.order.index') }}" class="dropdown-item"><i
+                      class="bi bi-clipboard-check me-1"></i> 我的订单</a></li>
+                <li><a href="{{ shop_route('account.wishlist.index') }}" class="dropdown-item"><i
+                      class="bi bi-heart me-1"></i> 我的收藏</a></li>
+                <li>
+                  <hr class="dropdown-divider">
+                </li>
+                <li><a href="{{ shop_route('logout') }}" class="dropdown-item"><i class="bi bi-box-arrow-left me-1"></i>
+                    退出登录</a></li>
               @else
-                <li><a href="{{ shop_route('login.index') }}" class="dropdown-item"><i class="bi bi-box-arrow-right me-1"></i> 登录/注册</a></li>
+                <li><a href="{{ shop_route('login.index') }}" class="dropdown-item"><i
+                      class="bi bi-box-arrow-right me-1"></i> 登录/注册</a></li>
               @endauth
             </ul>
           </li>
           <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="offcanvas" href="#offcanvas-right-cart" role="button" aria-controls="offcanvasExample">
+            <a class="nav-link" data-bs-toggle="offcanvas" href="#offcanvas-right-cart" role="button"
+              aria-controls="offcanvasExample">
               <i class="iconfont">&#xe634;</i>
               <div class="navbar-icon-link-badge"></div>
             </a>
@@ -118,9 +136,9 @@
 
   <div class="offcanvas offcanvas-top" tabindex="-1" id="offcanvas-search-top" aria-labelledby="offcanvasTopLabel">
     <div class="offcanvas-header">
-      <input type="text" class="form-control input-group-lg border-0 fs-4" focus placeholder="Type your search here" aria-label="Type your search here" aria-describedby="button-addon2">
+      <input type="text" class="form-control input-group-lg border-0 fs-4" focus
+        placeholder="Type your search here" aria-label="Type your search here" aria-describedby="button-addon2">
       <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     </div>
   </div>
 </header>
-
