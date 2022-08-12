9999d99fd642558d2709e9c8e0bbfe8e0611f7bf
pushuo <229102104@qq.com>
2022-08-12 15:39:13 +0800

    https://guangdagit.com/beike/beikeshop/issues/73

--- resources/beike/admin/views/pages/customer_groups/index.blade.php
+++ resources/beike/admin/views/pages/customer_groups/index.blade.php
@@ -187,7 +187,7 @@
 
         deleteCustomer(id, index) {
           const self = this;
-          this.$confirm('确定要删除用户码？', '提示', {
+          this.$confirm('确定要删除用户组吗？', '提示', {
             confirmButtonText: '确定',
             cancelButtonText: '取消',
             type: 'warning'
