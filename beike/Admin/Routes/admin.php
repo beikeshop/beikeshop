ad26df2af77c2e312fb2e280b7d0ef0d4d0f2706
pushuo <229102104@qq.com>
2022-08-12 15:35:36 +0800

    https://guangdagit.com/beike/beikeshop/issues/53

--- beike/Admin/Routes/admin.php
+++ beike/Admin/Routes/admin.php
@@ -41,8 +41,8 @@ Route::prefix($adminName)
                 Route::post('design/builder/preview', [Controllers\DesignController::class, 'preview'])->name('design.module.preview');
 
                 Route::get('design_footer/builder', [Controllers\DesignFooterController::class, 'index'])->name('design_footer.index');
-                Route::put('design_footer/builder', [Controllers\designFooterController::class, 'update'])->name('design_footer.update');
-                Route::post('design_footer/builder/preview', [Controllers\designFooterController::class, 'preview'])->name('design_footer.module.preview');
+                Route::put('design_footer/builder', [Controllers\DesignFooterController::class, 'update'])->name('design_footer.update');
+                Route::post('design_footer/builder/preview', [Controllers\DesignFooterController::class, 'preview'])->name('design_footer.module.preview');
 
                 Route::put('edit', [Controllers\EditController::class, 'update'])->name('edit');
                 Route::get('edit/locale', [Controllers\EditController::class, 'locale'])->name('edit.locale');
