c4a82b6e9ddc273ebbe3e0103bb595056859036f
Edward Yang <yangjin@opencart.cn>
2022-08-12 14:57:05 +0800

    fixed 404 page

--- themes/default/errors/404.blade.php
+++ themes/default/errors/404.blade.php
@@ -1,4 +1,4 @@
-@extends('layout.master')
+@extends('errors::minimal')
 
 @section('title', __('Not Found'))
 @section('code', '404')
--- themes/default/errors/500.blade.php
+++ themes/default/errors/500.blade.php
@@ -3,4 +3,3 @@
 @section('title', __('Server Error'))
 @section('code', '500')
 @section('message', __('Server Error'))
-/themes/default/errors/500
