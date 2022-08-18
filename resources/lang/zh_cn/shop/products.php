0ddfde5e0c4cf62f1c4c5164526a23938e0753ae
Edward Yang <yangjin@opencart.cn>
2022-08-18 08:32:13 +0800

    删除 diglactic/laravel-breadcrumbs

---
+++ resources/lang/zh_cn/shop/products.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * products.php
+ *
+ * @copyright  2022 opencart.cn - All Rights Reserved
+ * @link       http://www.guangdawangluo.com
+ * @author     Edward Yang <yangjin@opencart.cn>
+ * @created    2022-08-18 08:31:03
+ * @modified   2022-08-18 08:31:03
+ */
+
+return [
+    'search' => '搜索',
+];
