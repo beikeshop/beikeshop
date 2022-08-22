8bc503482f7a264d488b448cce301adc896d4893
Edward Yang <yangjin@opencart.cn>
2022-08-22 20:31:07 +0800

    fixed remove from wishlist

--- resources/lang/en/shop/wishlist.php
+++ resources/lang/en/shop/wishlist.php
@@ -11,5 +11,5 @@
 
 return [
     'add_wishlist_success' => 'Add to wishlist successfully',
-    'remove_wishlist_success' => 'Remove to wishlist successfully',
+    'remove_wishlist_success' => 'Remove from wishlist successfully',
 ];
