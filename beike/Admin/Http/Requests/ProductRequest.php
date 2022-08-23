d66566fe041bb4526bef1bd92d70c141118aece4
TL <mengwb@opencart.cn>
2022-08-23 15:19:54 +0800

    后台商品创建

--- beike/Admin/Http/Requests/ProductRequest.php
+++ beike/Admin/Http/Requests/ProductRequest.php
@@ -44,10 +44,10 @@ class ProductRequest extends FormRequest
     public function attributes()
     {
         return [
-            'descriptions.*.name' => trans('admin::product.name'),
-            'brand_id' => trans('admin::product.brand'),
-            'skus.*.sku' => trans('admin::product.sku'),
-            'skus.*.price' => trans('admin::product.price')
+            'descriptions.*.name' => trans('admin/product.name'),
+            'brand_id' => trans('admin/product.brand'),
+            'skus.*.sku' => trans('admin/product.sku'),
+            'skus.*.price' => trans('admin/product.price')
         ];
     }
 }
