c7fc8937b1393fc2945f3ebf94fceb5d26a046a9
pushuo <229102104@qq.com>
2022-09-01 12:01:23 +0800

    后台多语言优化

--- resources/beike/admin/views/pages/currencies/index.blade.php
+++ resources/beike/admin/views/pages/currencies/index.blade.php
@@ -138,7 +138,7 @@
 
           this.$refs[form].validate((valid) => {
             if (!valid) {
-              this.$message.error('请检查表单是否填写正确');
+              this.$message.error('{{ __('common.error_form')) }}');
               return;
             }
 
@@ -157,9 +157,9 @@
 
         deleteCustomer(id, index) {
           const self = this;
-          this.$confirm('确定要删除货币吗？', '提示', {
-            confirmButtonText: '确定',
-            cancelButtonText: '取消',
+          this.$confirm('{{ __('common.confirm_delete') }}', '{{ __('common.text_hint') }}', {
+            confirmButtonText: '{{ __('common.confirm') }}',
+            cancelButtonText: '{{ __('common.cancel') }}',
             type: 'warning'
           }).then(() => {
             $http.delete('currencies/' + id).then((res) => {
--- resources/beike/admin/views/pages/languages/index.blade.php
+++ resources/beike/admin/views/pages/languages/index.blade.php
@@ -141,7 +141,7 @@
 
           this.$refs[form].validate((valid) => {
             if (!valid) {
-              this.$message.error('请检查表单是否填写正确');
+              this.$message.error('{{ __('common.error_form') }}');
               return;
             }
 
@@ -160,9 +160,9 @@
 
         deleteCustomer(id, index) {
           const self = this;
-          this.$confirm('确定要删除语言吗？', '提示', {
-            confirmButtonText: '确定',
-            cancelButtonText: '取消',
+          this.$confirm('{{ __('common.confirm_delete') }}', '{{ __('common.text_hint') }}', {
+            confirmButtonText: '{{ __('common.confirm') }}',
+            cancelButtonText: '{{ __('common.cancel') }}',
             type: 'warning'
           }).then(() => {
             $http.delete('languages/' + id).then((res) => {
