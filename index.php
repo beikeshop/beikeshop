99ed2723464a1fa54b84df8268ccee812e6c4a45
Edward Yang <yangjin@opencart.cn>
2022-09-06 11:36:11 +0800

    修复网站根目录提示

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * index.php
+ *
+ * @copyright  2022 opencart.cn - All Rights Reserved
+ * @link       http://www.guangdawangluo.com
+ * @author     Edward Yang <yangjin@opencart.cn>
+ * @created    2022-09-06 10:50:56
+ * @modified   2022-09-06 10:50:56
+ */
+
+$currentDir = getcwd();
+die("请将网站根目录设置为: {$currentDir}/public/");
