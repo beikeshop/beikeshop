e9af38bda6a6ab6e316acf91ff53dc66130cf67b
pushuo <229102104@qq.com>
2022-09-08 10:10:23 +0800

    优化安装页面

--- beike/Installer/Views/finished.blade.php
+++ beike/Installer/Views/finished.blade.php
@@ -15,9 +15,14 @@
 
 		<div class="d-flex justify-content-center flex-column align-items-center">
 
-			<div class="welcome-img mb-5" style="max-width: 260px;"><img src="https://beikeshop.com/install/install-2.png?version={{ config('beike.version') }}&build_date={{ config('beike.build') }}" class="img-fluid"></div>
+			<div class="welcome-img mb-5" style="max-width: 240px;"><img src="https://beikeshop.com/install/install-2.png?version={{ config('beike.version') }}&build_date={{ config('beike.build') }}" class="img-fluid"></div>
 
-			<h5 class="text-center mb-5">{{ __('installer::installer_messages.final.finished') }}</h5>
+			<h5 class="text-center mb-4">{{ __('installer::installer_messages.final.finished') }}</h5>
+
+			<div class="mb-5 d-flex align-items-center">
+				<div>{{ trans('installer::installer_messages.environment.admin_email') }}：xxxxxxxx</div>
+				<div class="ms-5">{{ trans('installer::installer_messages.environment.admin_password') }}：xxxxxxxx</div>
+			</div>
 
 			<div class="d-flex justify-content-center">
 				<a href="{{ url('/') }}" class="btn btn-primary"><i class="bi bi-window-dock me-2"></i> {{ trans('installer::installer_messages.final.to_front') }}</a>
