2d86529eb8883413ecf17aae092a895be03035a2
Edward Yang <yangjin@opencart.cn>
2022-09-14 10:00:51 +0800

    fixed beikeshop

--- beike/Config/beike.php
+++ beike/Config/beike.php
@@ -1,12 +1,12 @@
 <?php
 /**
- * Resources.php
+ * Beike Config
  *
  * @copyright  2022 beikeshop.com - All Rights Reserved
  * @link       https://beikeshop.com
  * @author     Edward Yang <yangjin@guangda.work>
- * @created    2022-05-09 10:32:41
- * @modified   2022-05-09 10:32:41
+ * @created    2022-06-06 09:09:09
+ * @modified   2022-09-13 22:32:41
  */
 
 return [
