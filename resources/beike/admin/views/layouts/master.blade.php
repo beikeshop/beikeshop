b585cc5d7a9312d3b153e0bf64cab1f9ce196f25
pushuo <229102104@qq.com>
2022-09-16 18:36:08 +0800

    后台底部优化

--- resources/beike/admin/views/layouts/master.blade.php
+++ resources/beike/admin/views/layouts/master.blade.php
@@ -42,7 +42,7 @@
       <div class="container-fluid p-0">
         @yield('content')
 
-        <p class="text-center text-secondary mt-5">Chengdu Guangda Network Technology &copy; {{ date('Y') }} All Rights Reserved v{{ config('beike.version') }}({{ config('beike.build') }})</p>
+        <p class="text-center text-secondary mt-5">Chengdu <a href="https://beikeshop.com/" class="ms-2" target="_blank">Guangda Network</a> Technology &copy; {{ date('Y') }} All Rights Reserved v{{ config('beike.version') }}({{ config('beike.build') }})</p>
       </div>
     </div>
   </div>
