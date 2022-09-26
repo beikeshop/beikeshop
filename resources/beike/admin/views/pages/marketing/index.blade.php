cac7d30e0a360da7424f88c509e00c1d2970d2dd
pushuo <pushuo@guangda.work>
2022-09-26 19:13:44 +0800

    wip

--- resources/beike/admin/views/pages/marketing/index.blade.php
+++ resources/beike/admin/views/pages/marketing/index.blade.php
@@ -11,34 +11,15 @@
         <div class="row">
           <div class="col-xxl-20 col-xl-3 col-lg-4 col-md-4 d-flex align-items-center mb-3">
             <label class="filter-title">{{ __('product.name') }}</label>
-            <input @keyup.enter="search" type="text" v-model="filter.name" class="form-control" placeholder="name">
+            <input @keyup.enter="search" type="text" v-model="filter.keyword" class="form-control" placeholder="name">
           </div>
-          {{-- <div class="col-xxl-20 col-xl-3 col-lg-4 col-md-4 d-flex align-items-center mb-3">
-            <label class="filter-title">{{ __('product.sku') }}</label>
-            <input @keyup.enter="search" type="text" v-model="filter.sku" class="form-control" placeholder="sku">
-          </div>
-
           <div class="col-xxl-20 col-xl-3 col-lg-4 col-md-4 d-flex align-items-center mb-3">
-            <label class="filter-title">{{ __('product.model') }}</label>
-            <input @keyup.enter="search" type="text" v-model="filter.model" class="form-control" placeholder="model">
-          </div> --}}
-{{--
-          <div class="col-xxl-20 col-xl-3 col-lg-4 col-md-4 d-flex align-items-center mb-3">
-            <label class="filter-title">{{ __('product.category') }}</label>
-            <select v-model="filter.category_id" class="form-control">
+            <label class="filter-title">{{ __('admin/tax_rate.type') }}</label>
+            <select v-model="filter.type" class="form-control">
               <option value="">{{ __('common.all') }}</option>
-              @foreach ($categories as $_category)
-                <option :value="{{ $_category->id }}">{{ $_category->name }}</option>
+              @foreach ($types as $type)
+                <option value="{{ $type['value'] }}">{{ $type['label'] }}</option>
               @endforeach
-            </select>
-          </div> --}}
-
-          <div class="col-xxl-20 col-xl-3 col-lg-4 col-md-4 d-flex align-items-center mb-3">
-            <label class="filter-title">{{ __('common.status') }}</label>
-            <select v-model="filter.active" class="form-control">
-              <option value="">{{ __('common.all') }}</option>
-              <option value="1">{{ __('product.active') }}</option>
-              <option value="0">{{ __('product.inactive') }}</option>
             </select>
           </div>
         </div>
@@ -69,8 +50,8 @@
         </div>
       </div>
 
-      <el-pagination v-if="plugins.data.length" layout="prev, pager, next" background :page-size="plugins.per_page" :current-page.sync="page"
-        :total="plugins.total"></el-pagination>
+      <el-pagination v-if="plugins.data.length" layout="prev, pager, next" background :page-size="plugins.meta.per_page" :current-page.sync="page"
+        :total="plugins.meta.total"></el-pagination>
     </div>
   </div>
 @endsection
@@ -85,11 +66,8 @@
       page: 1,
 
       filter: {
-        name: bk.getQueryString('name'),
-        category_id: bk.getQueryString('category_id'),
-        sku: bk.getQueryString('sku'),
-        model: bk.getQueryString('model'),
-        active: bk.getQueryString('active'),
+        keyword: bk.getQueryString('keyword'),
+        type: bk.getQueryString('type'),
       },
     },
 
