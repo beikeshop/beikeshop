d6263e6db69b5f39694bbb623fae1ac1f506caba
Edward Yang <yangjin@guangda.work>
2022-09-26 15:45:57 +0800

    远程下载安装插件

--- beike/Admin/Http/Controllers/MarketingController.php
+++ beike/Admin/Http/Controllers/MarketingController.php
@@ -11,6 +11,7 @@
 
 namespace Beike\Admin\Http\Controllers;
 
+use ZanySoft\Zip\Zip;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Beike\Admin\Services\MarketingService;
@@ -36,15 +37,21 @@ class MarketingController
      */
     public function download(Request $request)
     {
-        $pluginCode = $request->code;
-        $datetime = date('Y-m-d');
-        $url = config('beike.api_url') . "/api/plugins/{$pluginCode}/download";
-        $content = file_get_contents($url);
+        try {
+            $pluginCode = $request->code;
+            $datetime = date('Y-m-d');
+            $url = config('beike.api_url') . "/api/plugins/{$pluginCode}/download";
+            $content = file_get_contents($url);
 
-        $pluginPath = "plugins/{$pluginCode}-{$datetime}.zip";
-        Storage::disk('local')->put($pluginPath, $content);
+            $pluginPath = "plugins/{$pluginCode}-{$datetime}.zip";
+            Storage::disk('local')->put($pluginPath, $content);
 
-        $pluginZip = storage_path('app' . $pluginPath);
-        dd($pluginZip);
+            $pluginZip = storage_path('app/' . $pluginPath);
+            $zipFile = Zip::open($pluginZip);
+            $zipFile->extract(base_path('plugins'));
+            return json_success('下载解压成功, 请去插件列表安装');
+        } catch (\Exception $e) {
+            return json_fail($e->getMessage());
+        }
     }
 }
