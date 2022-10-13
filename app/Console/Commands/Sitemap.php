e77f861efb8f981dab7cc9ece06f5141091f0389
TL <mengwb@guangda.work>
2022-10-13 16:06:35 +0800

    sitemap

---
+++ app/Console/Commands/Sitemap.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Beike\Services\SitemapService;
+use Illuminate\Console\Command;
+
+class Sitemap extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sitemap:generate';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '生成网站站点地图sitemap.xml文件';
+
+    /**
+     * Execute the console command.
+     *
+     */
+    public function handle()
+    {
+        SitemapService::gen();
+        dump('Success!');
+    }
+}
