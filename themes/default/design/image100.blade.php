3b5917d27f05d9e2228faac8868a6571814a1714
pushuo <pushuo@guangda.work>
2022-11-16 09:11:24 +0800

    优化横幅模块在大分辨率屏幕下的居中问题 -> https://gitee.com/beikeshop/beikeshop/issues/I61CZO?from=project-issue

--- themes/default/design/image100.blade.php
+++ themes/default/design/image100.blade.php
@@ -12,7 +12,7 @@
 
   <div class="module-image-plus module-info {{ !$content['full'] ? 'container' : '' }} mb-3 mb-md-5 d-flex justify-content-center">
     <div class="container{{ $content['full'] ? '-fluid' : '' }} d-flex justify-content-center">
-      <a class="col-12" href="{{ $content['images'][0]['link']['link'] ?? '' }}"><img src="{{ $content['images'][0]['image'] }}" class="img-fluid"></a>
+      <a href="{{ $content['images'][0]['link']['link'] ?? '' }}"><img src="{{ $content['images'][0]['image'] }}" class="img-fluid"></a>
     </div>
   </div>
 </section>
