17e2af1e2a1695e29bca0424710cd5e67c1c351a
pushuo <pushuo@guangda.work>
2023-01-09 09:54:15 +0800

    优化邮件 logo 显示问题

--- themes/default/layout/mail.blade.php
+++ themes/default/layout/mail.blade.php
@@ -6,13 +6,13 @@
           <tr style="font-weight:300">
             <td style="width:3%;max-width:30px;"></td>
             <td style="max-width:600px;">
-              <div style="width:92px; height:25px;">
-                <a href="{{ shop_route('home.index') }}">
-                  <img border="0" src="{{ image_origin(system_setting('base.logo')) }}" style="max-width:100%; height: auto;">
+              <div style="height: 35px;display: block;max-width: 200px;">
+                <a href="{{ shop_route('home.index') }}" style="display: block;height: 35px;display: block;max-width: 200px;">
+                  <img border="0" src="{{ image_origin(system_setting('base.logo')) }}" style="max-width:100%; max-height: 100%;display">
                 </a>
               </div>
 
-              <p style="height:2px;background-color: #fd560f;border: 0;font-size:0;padding:0;width:100%;margin-top:20px;"></p>
+              <p style="height:2px;background-color: #fd560f;border: 0;font-size:0;padding:0;width:100%;margin-top:10px;"></p>
               <div style="background-color:#fff; padding:23px 0 20px;box-shadow: 0px 1px 1px 0px rgba(122, 55, 55, 0.2);text-align:left;">
                 <table style="width:100%;font-weight:300;margin-bottom:10px;border-collapse:collapse;text-align:left;">
                   @yield('content')
