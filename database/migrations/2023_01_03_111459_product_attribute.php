1a53a7fbebf907c02d7adc08ed52b9dfed752684
Edward Yang <yangjin@guangda.work>
2023-01-13 17:23:04 +0800

    完善产品属性迁移脚本

--- database/migrations/2023_01_03_111459_product_attribute.php
+++ database/migrations/2023_01_03_111459_product_attribute.php
@@ -14,12 +14,14 @@ return new class extends Migration
     public function up()
     {
         Schema::create('attributes', function (Blueprint $table) {
+            $table->comment('属性表');
             $table->id();
             $table->unsignedInteger('attribute_group_id')->comment('属性组 ID')->index('attribute_group_id');
             $table->integer('sort_order')->comment('排序');
             $table->timestamps();
         });
         Schema::create('attribute_descriptions', function (Blueprint $table) {
+            $table->comment('属性描述表');
             $table->id();
             $table->unsignedInteger('attribute_id')->comment('属性 ID')->index('attribute_id');
             $table->string('locale')->default('')->comment('语言');
@@ -28,11 +30,13 @@ return new class extends Migration
             $table->timestamps();
         });
         Schema::create('attribute_values', function (Blueprint $table) {
+            $table->comment('属性值表');
             $table->id();
             $table->unsignedInteger('attribute_id')->comment('属性 ID')->index('attribute_id');
             $table->timestamps();
         });
         Schema::create('attribute_value_descriptions', function (Blueprint $table) {
+            $table->comment('属性值描述表');
             $table->id();
             $table->unsignedInteger('attribute_value_id')->comment('属性值 ID')->index('attribute_value_id');
             $table->string('locale')->default('')->comment('语言');
@@ -41,11 +45,13 @@ return new class extends Migration
             $table->timestamps();
         });
         Schema::create('attribute_groups', function (Blueprint $table) {
+            $table->comment('属性组表');
             $table->id();
             $table->integer('sort_order')->comment('排序');
             $table->timestamps();
         });
         Schema::create('attribute_group_descriptions', function (Blueprint $table) {
+            $table->comment('属性组描述表');
             $table->id();
             $table->unsignedInteger('attribute_group_id')->comment('属性组 ID')->index('attribute_group_id');
             $table->string('locale')->default('')->comment('语言');
@@ -54,6 +60,7 @@ return new class extends Migration
             $table->timestamps();
         });
         Schema::create('product_attributes', function (Blueprint $table) {
+            $table->comment('产品属性关联表');
             $table->id();
             $table->unsignedInteger('product_id')->comment('商品 ID')->index('product_id');
             $table->unsignedInteger('attribute_id')->comment('属性 ID')->index('attribute_id');
