50ca3e4aff089ad691090ed2e5723b653acd4aae
pushuo <pushuo@guangda.work>
2023-01-13 12:21:06 +0800

    修复 stripe 插件前端 vue 引入链接错误

--- plugins/Stripe/Views/checkout/payment.blade.php
+++ plugins/Stripe/Views/checkout/payment.blade.php
@@ -1,4 +1,4 @@
-<script src="{{ asset('vendor/vue/2.6.14/vue.js') }}"></script>
+<script src="{{ asset('vendor/vue/2.7/vue' . (!config('app.debug') ? '.min' : '') . '.js') }}"></script>
 <script src="{{ asset('vendor/element-ui/2.15.6/js.js') }}"></script>
 <link rel="stylesheet" href="{{ asset('vendor/element-ui/2.15.6/css.css') }}">
 
