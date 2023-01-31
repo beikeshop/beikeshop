bd14ecf41b185bbc12f89fdc7339cdd275bf6006
pushuo <pushuo@guangda.work>
2023-01-31 15:04:51 +0800

    https://gitee.com/beikeshop/beikeshop/issues/I6BXVS?from=project-issue

--- themes/default/product.blade.php
+++ themes/default/product.blade.php
@@ -55,7 +55,7 @@
           <h1 class="mb-4 product-name">{{ $product['name'] }}</h1>
           <div class="price-wrap d-flex align-items-end">
             <div class="new-price fs-1 lh-1 fw-bold me-2">@{{ product.price_format }}</div>
-            <div class="old-price text-muted text-decoration-line-through" v-if="product.price != product.origin_price">
+            <div class="old-price text-muted text-decoration-line-through" v-if="product.price != product.origin_price && product.origin_price !== 0">
               @{{ product.origin_price_format }}
             </div>
           </div>
