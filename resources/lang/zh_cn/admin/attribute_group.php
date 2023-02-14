d0cee69fc6d9c8a8c7b297fb73531cc29357cdcc
Edward Yang <yangjin@guangda.work>
2023-02-14 11:24:25 +0800

    属性多语言

--- resources/lang/zh_cn/admin/attribute_group.php
+++ resources/lang/zh_cn/admin/attribute_group.php
@@ -14,9 +14,9 @@ return [
     'create_at_groups'                   => '创建属性组',
     'error_cannot_delete_attribute_used' => '属性组不能删除，由于该属性组被属性（属性ID: :attributes）使用',
 
-    'attribute_groups_index'  => '属性组列表',
-    'attribute_groups_create' => '创建属性组',
-    'attribute_groups_show'   => '属性组详情',
-    'attribute_groups_update' => '更新属性组',
-    'attribute_groups_delete' => '删除属性组',
+    'attribute_groups_index'             => '属性组列表',
+    'attribute_groups_create'            => '创建属性组',
+    'attribute_groups_show'              => '属性组详情',
+    'attribute_groups_update'            => '更新属性组',
+    'attribute_groups_delete'            => '删除属性组',
 ];
