ebc96147c50c70363e959d5aa49434fb418454a2
Edward Yang <yangjin@guangda.work>
2023-03-16 15:49:40 +0800

    Import demo data

--- resources/lang/zh_cn/admin/theme.php
+++ resources/lang/zh_cn/admin/theme.php
@@ -13,6 +13,6 @@ return [
     'page_title'             => '我的模板',
     'theme_name'             => '默认主题',
     'enabled_text'           => '当前启用',
-    'theme_pop_checkbox'     => '导入模板数据',
-    'theme_pop_text'         => '如果勾选导入模板数据，可能会覆盖现有配置的页面布局、分类、商品等数据，请先做好备份！',
+    'theme_pop_checkbox'     => '导入演示数据',
+    'theme_pop_text'         => '如果勾选导入演示数据，可能会覆盖现有配置的页面布局、分类、商品等数据，请先做好备份！',
 ];
--- resources/lang/zh_hk/admin/theme.php
+++ resources/lang/zh_hk/admin/theme.php
@@ -13,6 +13,6 @@ return [
     'page_title'             => '我的模板',
     'theme_name'             => '默認主題',
     'enabled_text'           => '當前啟用',
-    'theme_pop_checkbox'     => '導入模板數據',
-    'theme_pop_text'         => '如果勾選導入模板數據，可能會覆蓋現有配置的頁面佈局、分類、商品等數據，請先做好備份！ ',
+    'theme_pop_checkbox'     => '導入演示數據',
+    'theme_pop_text'         => '如果勾選導入演示數據，可能會覆蓋現有配置的頁面佈局、分類、商品等數據，請先做好備份！ ',
 ];
