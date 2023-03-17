423ed6a1457e2999246b33c7e22572a7d644a424
Edward Yang <yangjin@guangda.work>
2023-03-17 09:55:15 +0800

    update version and build

--- beike/Config/beike.php
+++ beike/Config/beike.php
@@ -11,8 +11,8 @@
 
 return [
     'api_url'         => env('BEIKE_API_URL', 'https://beikeshop.com'),
-    'version'         => '1.3.2',
-    'build'           => '20230301',
+    'version'         => '1.3.4',
+    'build'           => '20230317',
 
     'admin_name'      => env('ADMIN_NAME'),
     'force_url_https' => env('APP_FORCE_HTTPS', false),
