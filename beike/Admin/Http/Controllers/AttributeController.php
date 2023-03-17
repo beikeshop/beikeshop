e61c9a6def95c37bc85b502bbec9f722f2b3f40b
TL <mengwb@guangda.work>
2023-03-17 16:45:51 +0800

    错误处理
    wip

--- beike/Admin/Http/Controllers/AttributeController.php
+++ beike/Admin/Http/Controllers/AttributeController.php
@@ -17,21 +17,26 @@ use Beike\Admin\Http\Resources\AttributeValueResource;
 use Beike\Admin\Http\Resources\AutocompleteResource;
 use Beike\Admin\Repositories\AttributeGroupRepo;
 use Beike\Admin\Repositories\AttributeRepo;
+use Exception;
 use Illuminate\Http\Request;
 
 class AttributeController extends Controller
 {
     public function index(Request $request)
     {
-        $attributes = AttributeRepo::getList();
-        $data       = [
-            'attribute_list'        => $attributes,
-            'attribute_list_format' => AttributeResource::collection($attributes),
-            'attribute_group'       => AttributeGroupRepo::getList(),
-        ];
-        $data = hook_filter('admin.attribute.index.data', $data);
-        if ($request->expectsJson()) {
-            return json_success(trans('success'), $data);
+        try {
+            $attributes = AttributeRepo::getList();
+            $data = [
+                'attribute_list' => $attributes,
+                'attribute_list_format' => AttributeResource::collection($attributes),
+                'attribute_group' => AttributeGroupRepo::getList(),
+            ];
+            $data = hook_filter('admin.attribute.index.data', $data);
+            if ($request->expectsJson()) {
+                return json_success(trans('success'), $data);
+            }
+        } catch (Exception $e) {
+            return view('admin::pages.attributes.index', $data)->withErrors(['error' => $e->getMessage()]);
         }
 
         return view('admin::pages.attributes.index', $data);
@@ -39,71 +44,107 @@ class AttributeController extends Controller
 
     public function show(Request $request, int $id)
     {
-        $data = [
-            'attribute'       => (new AttributeDetailResource(AttributeRepo::find($id)))->jsonSerialize(),
-            'attribute_group' => AttributeGroupRepo::getList(),
-        ];
-        $data = hook_filter('admin.attribute.show.data', $data);
+        try {
+            $data = [
+                'attribute'       => (new AttributeDetailResource(AttributeRepo::find($id)))->jsonSerialize(),
+                'attribute_group' => AttributeGroupRepo::getList(),
+            ];
+            $data = hook_filter('admin.attribute.show.data', $data);
+        } catch (Exception $e) {
+            return view('admin::pages.attributes.form', $data)->withErrors(['error' => $e->getMessage()]);
+        }
 
         return view('admin::pages.attributes.form', $data);
     }
 
     public function store(Request $request)
     {
-        $requestData = json_decode($request->getContent(), true);
-        $item        = AttributeRepo::create($requestData);
+        try {
+            $requestData = json_decode($request->getContent(), true);
+            $item        = AttributeRepo::create($requestData);
+        } catch (Exception $e) {
+            return json_fail($e->getMessage(), []);
+        }
 
         return json_success(trans('common.created_success'), $item);
     }
 
     public function update(Request $request, int $id)
     {
-        $requestData = json_decode($request->getContent(), true);
-        $item        = AttributeRepo::update($id, $requestData);
+        try {
+            $requestData = json_decode($request->getContent(), true);
+            $item        = AttributeRepo::update($id, $requestData);
+        } catch (Exception $e) {
+            return json_fail($e->getMessage(), []);
+        }
 
         return json_success(trans('common.updated_success'), $item);
     }
 
     public function storeValue(Request $request, int $id)
     {
-        $requestData = json_decode($request->getContent(), true);
-        $item        = AttributeRepo::createValue(array_merge($requestData, ['attribute_id' => $id]));
+        try {
+            $requestData = json_decode($request->getContent(), true);
+            $item        = AttributeRepo::createValue(array_merge($requestData, ['attribute_id' => $id]));
+        } catch (Exception $e) {
+            return json_fail($e->getMessage(), []);
+        }
 
         return json_success(trans('common.created_success'), new AttributeValueResource($item));
     }
 
     public function updateValue(Request $request, int $id, int $value_id)
     {
-        $requestData = json_decode($request->getContent(), true);
-        $item        = AttributeRepo::updateValue($value_id, $requestData);
+        try {
+            $requestData = json_decode($request->getContent(), true);
+            $item        = AttributeRepo::updateValue($value_id, $requestData);
+        } catch (Exception $e) {
+            return json_fail($e->getMessage(), []);
+        }
 
         return json_success(trans('common.updated_success'), new AttributeValueResource($item));
     }
 
     public function destroyValue(Request $request, int $id, int $value_id)
     {
-        AttributeRepo::deleteValue($value_id);
+        try {
+            AttributeRepo::deleteValue($value_id);
+        } catch (Exception $e) {
+            return json_fail($e->getMessage(), []);
+        }
 
         return json_success(trans('common.deleted_success'));
     }
 
     public function destroy(Request $request, int $id)
     {
-        AttributeRepo::delete($id);
+        try {
+            AttributeRepo::delete($id);
+        } catch (Exception $e) {
+            return json_fail($e->getMessage(), []);
+        }
 
         return json_success(trans('common.deleted_success'));
     }
 
     public function autocomplete(Request $request): array
     {
-        $items = AttributeRepo::autocomplete($request->get('name') ?? '', 0);
+        try {
+            $items = AttributeRepo::autocomplete($request->get('name') ?? '', 0);
+        } catch (Exception $e) {
+            return json_fail($e->getMessage(), []);
+        }
 
         return json_success(trans('common.get_success'), AutocompleteResource::collection($items));
     }
 
     public function autocompleteValue(Request $request, int $id): array
     {
-        $items = AttributeRepo::autocompleteValue($id, $request->get('name') ?? '');
+        try {
+            $items = AttributeRepo::autocompleteValue($id, $request->get('name') ?? '');
+        } catch (Exception $e) {
+            return json_fail($e->getMessage(), []);
+        }
 
         return json_success(trans('common.get_success'), AutocompleteResource::collection($items));
     }
