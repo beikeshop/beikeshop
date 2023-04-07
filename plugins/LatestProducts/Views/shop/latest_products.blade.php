b5be111f412eba73b99cf5e7f0a62fd8962c8e52
蒲硕 <pushuo@guangda.work>
2023-04-07 02:03:23 +0000

    !73 Optimize the breadcrumbs of the latest product plugin * Optimize the breadcrumbs of the latest product plugin

--- plugins/LatestProducts/Views/shop/latest_products.blade.php
+++ plugins/LatestProducts/Views/shop/latest_products.blade.php
@@ -1,22 +1,21 @@
 @extends('layout.master')
-
 @section('content')
-    <div class="container">
-        <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="/">@lang('shop/common.home')</a></li>
-                <li class="breadcrumb-item active" aria-current="page">{{ __('LatestProducts::header.latest_products') }}</li>
-            </ol>
-        </nav>
-
-        <div class="row">
-            @foreach ($items as $product)
-                <div class="col-6 col-md-3">@include('shared.product')</div>
-            @endforeach
-        </div>
-
-        {{ $products->links('shared/pagination/bootstrap-4') }}
-
-    </div>
-
+<div class="breadcrumb-wrap">
+  <div class="container">
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="/">@lang('shop/common.home')</a></li>
+        <li class="breadcrumb-item active" aria-current="page">{{ __('LatestProducts::header.latest_products') }}</li>
+      </ol>
+    </nav>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    @foreach ($items as $product)
+    <div class="col-6 col-md-3">@include('shared.product')</div>
+    @endforeach
+  </div>
+  {{ $products->links('shared/pagination/bootstrap-4') }}
+</div>
 @endsection
