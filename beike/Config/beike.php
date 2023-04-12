c4858b587578e990875022317edb1b12a140600e
Edward Yang <yangjin@guangda.work>
2023-04-12 09:25:57 +0800

    update build number

--- beike/Config/beike.php
+++ beike/Config/beike.php
@@ -12,7 +12,7 @@
 return [
     'api_url'         => env('BEIKE_API_URL', 'https://beikeshop.com'),
     'version'         => '1.3.5',
-    'build'           => '20230407',
+    'build'           => '20230412',
 
     'admin_name'      => env('ADMIN_NAME'),
     'force_url_https' => env('APP_FORCE_HTTPS', false),
