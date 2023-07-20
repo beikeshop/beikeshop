af1eb9b9f32bcd591d186120bca10ad62639949f
pushuo <pushuo@guangda.work>
2023-07-20 15:11:24 +0800

    Fix footer logo link error

--- themes/default/layout/footer.blade.php
+++ themes/default/layout/footer.blade.php
@@ -32,7 +32,7 @@
       <div class="row">
         <div class="col-12 col-md-3">
           <div class="footer-content-left">
-            <div class="logo"><a href="http://"><img
+            <div class="logo"><a href="{{ shop_route('home.index') }}"><img
                   src="{{ image_origin($footer_content['content']['intro']['logo']) }}" class="img-fluid"></a></div>
             <div class="text tinymce-format-p">{!! $footer_content['content']['intro']['text'][$locale] ?? '' !!}</div>
           </div>
