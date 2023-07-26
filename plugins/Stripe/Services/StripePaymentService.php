e6f24d21186efce027343fa8af9b341fac4798d4
Edward Yang <yangjin@guangda.work>
2023-07-26 12:23:48 +0800

    create address for stripe customer

--- plugins/Stripe/Services/StripePaymentService.php
+++ plugins/Stripe/Services/StripePaymentService.php
@@ -13,7 +13,6 @@ namespace Plugin\Stripe\Services;
 
 use Beike\Shop\Services\PaymentService;
 use Stripe\Exception\ApiErrorException;
-use Stripe\Stripe;
 
 class StripePaymentService extends PaymentService
 {
@@ -30,27 +29,28 @@ class StripePaymentService extends PaymentService
     public function capture($creditCardData): bool
     {
         $apiKey = plugin_setting('stripe.secret_key');
-        Stripe::setApiKey($apiKey);
-        $tokenId  = $creditCardData['token'] ?? '';
+        \Stripe\Stripe::setApiKey($apiKey);
+        $tokenId = $creditCardData['token'] ?? '';
         if (empty($tokenId)) {
             throw new \Exception('Invalid token');
         }
         $currency = $this->order->currency_code;
 
-        if (! in_array($currency, self::ZERO_DECIMAL)) {
+        if (!in_array($currency, self::ZERO_DECIMAL)) {
             $total = round($this->order->total, 2) * 100;
         } else {
             $total = floor($this->order->total);
         }
 
+        $stripeCustomer = $this->createCustomer($tokenId);
+
         $stripeChargeParameters = [
-            'amount'   => $total,
+            'amount' => $total,
             'currency' => $currency,
             'metadata' => [
                 'orderId' => $this->order->id,
             ],
-            'source'   => $tokenId,
-            // 'customer' => $this->createCustomer(),
+            'customer' => $stripeCustomer->id,
         ];
 
         $charge = \Stripe\Charge::create($stripeChargeParameters);
@@ -60,15 +60,41 @@ class StripePaymentService extends PaymentService
 
     /**
      * 创建 stripe customer
-     * @return mixed
+     * @param string $source
+     * @return \Stripe\Customer
      * @throws ApiErrorException
      */
-    private function createCustomer(): mixed
+    private function createCustomer(string $source = ''): \Stripe\Customer
     {
-        $customer = \Stripe\Customer::create([
+        return \Stripe\Customer::create([
+            'source' => $source,
             'email' => $this->order->email,
+            'description' => setting('base.meta_title'),
+            'name' => $this->order->customer_name,
+            'phone' => $this->order->shipping_telephone,
+            'address' => [
+                'city' => $this->order->payment_city,
+                'country' => $this->order->payment_country,
+                'line1' => $this->order->payment_address_1,
+                'line2' => $this->order->payment_address_2,
+                'postal_code' => $this->order->payment_zipcode,
+                'state' => $this->order->payment_zone,
+            ],
+            'shipping' => [
+                'name' => $this->order->shipping_customer_name,
+                'phone' => $this->order->shipping_telephone,
+                'address' => [
+                    'city' => $this->order->shipping_city,
+                    'country' => $this->order->shipping_country,
+                    'line1' => $this->order->shipping_address_1,
+                    'line2' => $this->order->shipping_address_2,
+                    'postal_code' => $this->order->shipping_zipcode,
+                    'state' => $this->order->shipping_zone,
+                ],
+            ],
+            'metadata' => [
+                'order_number' => $this->order->number,
+            ],
         ]);
-
-        return $customer['id'];
     }
 }
