832b4275d43f3d42f88e648b21f91ce26b866f66
Edward Yang <yangjin@guangda.work>
2023-09-08 18:11:23 +0800

    完善语言包

--- resources/lang/zh_hk/admin/admin_roles.php
+++ resources/lang/zh_hk/admin/admin_roles.php
@@ -1,20 +1,21 @@
 <?php
 /**
- * header.php
+ * Lang.php
  *
- * @copyright  2022 beikeshop.com - All Rights Reserved
+ * @copyright  2023 beikeshop.com - All Rights Reserved
  * @link       https://beikeshop.com
  * @author     Edward Yang <yangjin@guangda.work>
- * @created    2022-08-02 19:03:19
- * @modified   2022-08-02 19:03:19
+ * @created    2023-09-09 09:09:09
+ * @modified   2023-09-08 10:08:05
  */
 
 return [
-    'select_all'             => '選中所有',
-    'unselect_all'           => '取消選中',
-    'role_name'              => '角色名稱',
-    'role_management'        => '角色管理',
-    'permission'             => '系統權限',
-    'plugin_permission'      => '插件權限',
-    'error_roles'            => '請至少選擇一個角色',
+    'error_roles'       => '請至少選擇一個角色',
+    'permission'        => '系統權限',
+    'plugin_permission' => '插件權限',
+    'role'              => '角色',
+    'role_management'   => '角色管理',
+    'role_name'         => '角色名稱',
+    'select_all'        => '選中所有',
+    'unselect_all'      => '取消選中',
 ];
