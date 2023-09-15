1a3a15492a71855ad895ebb355a425e0d2c5e9ba
Edward Yang <yangjin@guangda.work>
2023-09-15 17:25:10 +0800

    完善优惠券韩语

--- resources/lang/ko/shop/account.php
+++ resources/lang/ko/shop/account.php
@@ -1,35 +1,35 @@
 <?php
 /**
- * account.php
+ * Lang.php
  *
- * @copyright  2022 beikeshop.com - All Rights Reserved
+ * @copyright  2023 beikeshop.com - All Rights Reserved
  * @link       https://beikeshop.com
  * @author     Edward Yang <yangjin@guangda.work>
- * @created    2022-08-04 10:59:15
- * @modified   2022-08-04 10:59:15
+ * @created    2023-09-09 09:09:09
+ * @modified   2023-09-15 09:23:29
  */
 
 return [
-    'index'               => '개인중심',
-    'revise_info'         => '정보 수정',
-    'collect'             => '소장',
-    '쿠폰'                  => '쿠폰',
-    'my_order'            => '내 주문',
-    'orders'              => '전체주문',
-    'pending_payment'     => '미지급금',
-    'pending_send'        => '출하 대기 중',
-    'pending_receipt'     => '수령대기',
+    'account_not_exist'   => '계정 없음',
     'after_sales'         => 'A/S',
-    'no_order'            => '아직 주문이 없습니다!',
-    'to_buy'              => '주문하러 가기',
-    'order_number'        => '주문번호',
-    'order_time'          => '주문시간',
-    'state'               => '상태',
+    'all'                 => '공',
     'amount'              => '금액',
     'check_details'       => '상세보기',
-    'all'                 => '공',
+    'collect'             => '소장',
+    'coupon'              => '쿠폰',
+    'index'               => '개인중심',
     'items'               => '상품',
-    'verify_code_expired' => '인증번호가 만료되었습니다(10분). 다시 가져오십시오.',
+    'my_order'            => '내 주문',
+    'no_order'            => '아직 주문이 없습니다!',
+    'order_number'        => '주문번호',
+    'order_time'          => '주문시간',
+    'orders'              => '전체주문',
+    'pending_payment'     => '미지급금',
+    'pending_receipt'     => '수령대기',
+    'pending_send'        => '출하 대기 중',
+    'revise_info'         => '정보 수정',
+    'state'               => '상태',
+    'to_buy'              => '주문하러 가기',
     'verify_code_error'   => '인증번호 오류',
-    'account_not_exist'   => '계정 없음',
+    'verify_code_expired' => '인증번호가 만료되었습니다(10분). 다시 가져오십시오.',
 ];
