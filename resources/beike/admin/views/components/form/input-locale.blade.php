fe23e54b7db696299eef0658030b6f32d7958b8a
pushuo <pushuo@guangda.work>
2023-10-12 10:08:26 +0000

    !231 Fixed the problem of clearing data when submitting the multi-language input box in the background * Fixed the problem of clearing data when submitting the multi-language …

--- resources/beike/admin/views/components/form/input-locale.blade.php
+++ resources/beike/admin/views/components/form/input-locale.blade.php
@@ -4,10 +4,14 @@
     <div class="d-flex wp-{{ $width }} input-for-group">
       <span class="input-group-text wp-100 px-1" id="basic-addon1">{{ $locale['name'] }}</span>
       <input type="text" name="{{ $formatName($locale['code']) }}" value="{{ $formatValue($locale['code']) }}"
-        class="form-control short input-{{ $locale['code'] }}" placeholder="{{ $placeholder ?: $locale['name'] }}" @if ($required) required @endif>
-      <span class="invalid-feedback w-auto"
-        style="white-space:nowrap;">{{ __('common.error_required', ['name' => $title]) }}</span>
+        class="form-control short input-{{ $locale['code'] }} {{ $errors->first("descriptions.{$locale['code']}.name") ? 'is-invalid' : '' }}" placeholder="{{ $placeholder ?: $locale['name'] }}" @if ($required) required @endif>
+      @if (!$errors->first("descriptions.{$locale['code']}.name"))
+        <span class="invalid-feedback w-auto" style="white-space:nowrap;">{{ __('common.error_required', ['name' => $title]) }}</span>
+      @endif
     </div>
+    @if ($errors->first("descriptions.{$locale['code']}.name"))
+      <span class="invalid-feedback d-block">{{ $errors->first("descriptions.{$locale['code']}.name") }}</span>
+    @endif
     @if ($attributes->has('required'))
       @error($errorKey($locale['code']))
         <x-admin::form.error :message="$message" />
