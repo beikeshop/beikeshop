ddf92eaa0391f78d96b8f919481a863b8a6e18ef
Edward Yang <yangjin@guangda.work>
2023-10-18 15:59:57 +0800

    fixed weight library

---
+++ resources/lang/zh_cn/weight.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * weight.php
+ *
+ * @copyright  2023 beikeshop.com - All Rights Reserved
+ * @link       https://beikeshop.com
+ * @author     Edward Yang <yangjin@guangda.work>
+ * @created    2023-10-18 15:55:25
+ * @modified   2023-10-18 15:55:25
+ */
+
+return [
+    'kg' => '千克',
+    'g'  => '克',
+    'oz' => '盎司',
+    'lb' => '磅',
+    'ct' => '克拉',
+];
