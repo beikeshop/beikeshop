9a39a11bc2d228e2063d8054b077e0abda9f6aab
songliang <songliang@opencart.cn>
2023-11-30 15:27:18 +0800

    open new window

--- resources/beike/admin/views/pages/marketing/show.blade.php
+++ resources/beike/admin/views/pages/marketing/show.blade.php
@@ -110,7 +110,7 @@ $data = $plugin['data'];
             <div>
               <button class="btn btn-primary btn-lg" @click="downloadPlugin"><i class="bi bi-cloud-arrow-down-fill"></i> {{
                 __('admin/marketing.download_plugin') }}</button>
-              <a :href="toBkTicketUrl()" class="btn btn-primary btn-lg ms-2"><i class="bi bi-ticket-detailed-fill"></i> {{ __('admin/plugin.ticket') }}</a>
+              <a :href="toBkTicketUrl()" target="_blank" class="btn btn-primary btn-lg ms-2"><i class="bi bi-ticket-detailed-fill"></i> {{ __('admin/plugin.ticket') }}</a>
               @if (isset($data['plugin_services']) && count($data['plugin_services']) && $data['id'] !== 61 )
               <button class="btn btn-outline-primary btn-lg w-min-100 fw-bold ms-2" @click="openService">{{
                 __('admin/marketing.btn_buy_service') }}</button>
