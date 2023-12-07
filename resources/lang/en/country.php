4173b44697ee3b5d8206a7ac2b37f65fcb321739
songliang <songliang@opencart.cn>
2023-12-07 16:32:11 +0800

    语言包

---
+++ resources/lang/en/country.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Lang.php
+ *
+ * @copyright  2023 beikeshop.com - All Rights Reserved
+ * @link       https://beikeshop.com
+ * @author     Edward Yang <yangjin@guangda.work>
+ * @created    2023-09-09 09:09:09
+ * @modified   2023-12-07 08:31:52
+ */
+
+return [
+    'AF' => 'Africa',
+    'AN' => 'Antarctica',
+    'AS' => 'Asia',
+    'EU' => 'Europe',
+    'NA' => 'North America',
+    'OA' => 'Oceania',
+    'SA' => 'South America',
+    'none' => 'unknown',
+];
