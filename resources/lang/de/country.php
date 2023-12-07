8cdd1df6f8dfe94bc61ec90cc0c83b794d4d6264
songliang <songliang@opencart.cn>
2023-12-07 17:17:27 +0800

    语言包

---
+++ resources/lang/de/country.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Lang.php
+ *
+ * @copyright  2023 beikeshop.com - All Rights Reserved
+ * @link       https://beikeshop.com
+ * @author     Edward Yang <yangjin@guangda.work>
+ * @created    2023-09-09 09:09:09
+ * @modified   2023-12-07 09:13:39
+ */
+
+return [
+    'AF' => 'In afrika.',
+    'AN' => 'Ln der antarktis.',
+    'AS' => 'Asien.',
+    'EU' => 'europa',
+    'NA' => 'nordamerika',
+    'OA' => 'ozeanien',
+    'SA' => 'Nach südamerika.',
+    'none' => 'Unbekannt. - unbekannt.',
+];
---
+++ resources/lang/it/country.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Lang.php
+ *
+ * @copyright  2023 beikeshop.com - All Rights Reserved
+ * @link       https://beikeshop.com
+ * @author     Edward Yang <yangjin@guangda.work>
+ * @created    2023-09-09 09:09:09
+ * @modified   2023-12-07 09:15:37
+ */
+
+return [
+    'AF' => 'In africa',
+    'AN' => 'antartico',
+    'AS' => 'asia',
+    'EU' => 'europea',
+    'NA' => 'America settentrionale',
+    'OA' => 'oceania',
+    'SA' => 'America del sud',
+    'none' => 'sconosciuto',
+];
