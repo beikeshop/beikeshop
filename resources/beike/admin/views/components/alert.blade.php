3742456202f70f9ce52b6eeb885513417b6b5b79
pushuo <pushuo@guangda.work>
2024-01-11 10:10:17 +0800

    Optimize component alert

--- resources/beike/admin/views/components/alert.blade.php
+++ resources/beike/admin/views/components/alert.blade.php
@@ -1,5 +1,15 @@
 <div class="alert alert-{{ $type }} alert-dismissible">
-  <i class="bi bi-check-circle-fill"></i>
+  @php
+    $class = 'check-circle-fill';
+    if ($type == 'danger') {
+      $class = 'x-circle-fill';
+    } elseif ($type == 'warning') {
+      $class = 'exclamation-circle-fill';
+    } elseif ($type == 'info') {
+      $class = 'info-circle-fill';
+    }
+  @endphp
+  <i class="bi bi-{{ $class }}"></i>
   {!! $msg !!}
   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
