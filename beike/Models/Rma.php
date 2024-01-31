4fb3326b29d8d99611db36bd031af50a3096d7a3
TL <mengwb@guangda.work>
2024-01-31 14:34:28 +0800

    rma申请单增加图片附件

--- beike/Models/Rma.php
+++ beike/Models/Rma.php
@@ -19,7 +19,7 @@ class Rma extends Base
 {
     use HasFactory;
 
-    protected $fillable = ['order_id', 'order_product_id', 'customer_id', 'name', 'email', 'telephone', 'product_name', 'sku', 'quantity', 'json', 'opened', 'rma_reason_id', 'type', 'status', 'comment'];
+    protected $fillable = ['order_id', 'order_product_id', 'customer_id', 'name', 'email', 'telephone', 'product_name', 'sku', 'quantity', 'images', 'opened', 'rma_reason_id', 'type', 'status', 'comment'];
     protected $casts = ['images' => 'json'];
 
     public function order(): BelongsTo
