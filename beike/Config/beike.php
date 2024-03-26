0133a20138844c70c4b5fede0b6439ce428e400c
pushuo <pushuo@guangda.work>
2024-03-26 16:53:45 +0800

    优化插件市场购买按钮逻辑
    wip
    
    wip
    
    up version
    
    wip

--- beike/Config/beike.php
+++ beike/Config/beike.php
@@ -11,8 +11,8 @@
 
 return [
     'api_url'         => env('BEIKE_API_URL', 'https://beikeshop.com'),
-    'version'         => '1.5.5',
-    'build'           => '20240325',
+    'version'         => '1.5.5.1',
+    'build'           => '20240326',
 
     'admin_name'      => env('ADMIN_NAME'),
     'force_url_https' => env('APP_FORCE_HTTPS', false),
