d605282511de2413531bbb09812936236412f0c2
mengwb <mengwb@guangda.work>
2024-04-11 18:11:49 +0800

    v1.5.4.2

--- beike/Config/beike.php
+++ beike/Config/beike.php
@@ -12,7 +12,7 @@
 return [
     'api_url'         => env('BEIKE_API_URL', 'https://beikeshop.com'),
     'version'         => '1.5.4.2',
-    'build'           => '20240326',
+    'build'           => '20240411',
 
     'admin_name'      => env('ADMIN_NAME'),
     'force_url_https' => env('APP_FORCE_HTTPS', false),
