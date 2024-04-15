829dfa980d50d21e9fa6f5ea8f8458ce1a043660
pushuo <pushuo@guangda.work>
2024-04-15 08:45:08 +0800

    https://guangdagit.com/beike/bugs/issues/146

--- resources/beike/admin/views/pages/setting.blade.php
+++ resources/beike/admin/views/pages/setting.blade.php
@@ -50,7 +50,7 @@
         <div class="tab-content">
           <div class="tab-pane fade show active" id="tab-general">
             @hook('admin.setting.general.before')
-            <x-admin-form-input name="meta_title" title="{{ __('admin/setting.meta_title') }}" value="{{ old('meta_title', system_setting('base.meta_title', '')) }}" />
+            <x-admin-form-input name="meta_title" title="{{ __('admin/setting.meta_title') }}" value="{{ old('meta_title', system_setting('base.meta_title', '')) }}" required />
             <x-admin-form-textarea name="meta_description" title="{{ __('admin/setting.meta_description') }}" value="{{ old('meta_description', system_setting('base.meta_description', '')) }}" />
             <x-admin-form-textarea name="meta_keywords" title="{{ __('admin/setting.meta_keywords') }}" value="{{ old('meta_keywords', system_setting('base.meta_keywords', '')) }}" />
             <x-admin-form-input name="telephone" title="{{ __('admin/setting.telephone') }}" value="{{ old('telephone', system_setting('base.telephone', '')) }}" />
