c13cbab3be965229f572e56847e78c798b1ae612
蒋勇 <jiangy@highzap.com>
2024-06-22 18:28:45 +0800

    chore: 将Clarity跟踪代码输入框改为文本域

--- plugins/Clarity/columns.php
+++ plugins/Clarity/columns.php
@@ -7,7 +7,7 @@ return [
     [
         'name' => 'value',
         'label_key' => 'common.script_title',
-        'type' => 'string',
+        'type' => 'textarea',
         'required' => true,
     ],
 ];
