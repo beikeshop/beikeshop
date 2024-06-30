c7add97a19ea4ba03dbe40d52b11f2569a056a6b
jiangy <778671998@qq.com>
2024-06-30 23:48:50 +0800

    feat: 增加elementui 组件示例

--- plugins/Upsell/Views/admin/config_form.blade.php
+++ plugins/Upsell/Views/admin/config_form.blade.php
@@ -1,18 +1,18 @@
 @section('page-title-right')
-<button type="button" class="btn btn-primary save-btn" onclick="app.submit('form')">{{ __('common.save') }}</button>
+  <button type="button" class="btn btn-primary save-btn" onclick="app.submit('form')">{{ __('common.save') }}</button>
 @endsection
 <div class="mb-5" id="app">
   自定义的插件设置入口
   <table class="table">
-  <thead>
+    <thead>
     <tr>
       <th scope="col">#</th>
       <th scope="col">First</th>
       <th scope="col">Last</th>
       <th scope="col">Handle</th>
     </tr>
-  </thead>
-  <tbody>
+    </thead>
+    <tbody>
     <tr>
       <th scope="row">1</th>
       <td>Mark</td>
@@ -30,23 +30,98 @@
       <td colspan="2">Larry the Bird</td>
       <td>@twitter</td>
     </tr>
-  </tbody>
-</table>
+    </tbody>
+  </table>
+  {{-- elementui table--}}
+  <el-card>
+    <el-button @click="resetDateFilter">清除日期过滤器</el-button>
+    <el-button @click="clearFilter">清除所有过滤器</el-button>
+    <el-table
+      ref="filterTable"
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        sortable
+        width="180"
+        column-key="date"
+        :filters="[{text: '2016-05-01', value: '2016-05-01'}, {text: '2016-05-02', value: '2016-05-02'}, {text: '2016-05-03', value: '2016-05-03'}, {text: '2016-05-04', value: '2016-05-04'}]"
+        :filter-method="filterHandler"
+      >
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址"
+        :formatter="formatter">
+      </el-table-column>
+      <el-table-column
+        prop="tag"
+        label="标签"
+        width="100"
+        :filters="[{ text: '家', value: '家' }, { text: '公司', value: '公司' }]"
+        :filter-method="filterTag"
+        filter-placement="bottom-end">
+        <template slot-scope="scope">
+          <el-tag
+            :type="scope.row.tag === '家' ? 'primary' : 'success'"
+            disable-transitions>@{{scope.row.tag}}</el-tag>
+        </template>
+      </el-table-column>
+    </el-table>
+  </el-card>
 </div>
+<!-- <script src="https://unpkg.com/bootstrap-vue@latest/dist/bootstrap-vue.min.js"></script> -->
 <script>
   let app = new Vue({
     el: '#app',
     data() {
       return {
-        plugin: {
-          code: "upsell"
-        }
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+          tag: '家'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+          tag: '公司'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+          tag: '家'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+          tag: '公司'
+        }]
       }
     },
-    computed: {
-    },
-
     methods: {
+      resetDateFilter() {
+        this.$refs.filterTable.clearFilter('date');
+      },
+      clearFilter() {
+        this.$refs.filterTable.clearFilter();
+      },
+      formatter(row, column) {
+        return row.address;
+      },
+      filterTag(value, row) {
+        return row.tag === value;
+      },
+      filterHandler(value, row, column) {
+        const property = column['property'];
+        return row[property] === value;
+      },
       submit(form) {
         alert(`form submit ${this.plugin.code}`)
       }
