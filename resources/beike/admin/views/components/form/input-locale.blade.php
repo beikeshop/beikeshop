d67efc6c3fb4735c5f263868bae35de63a2a2323
pushuo <pushuo@guangda.work>
2024-07-09 14:25:15 +0800

    修复后台商品编辑，保存 SEO填写内容被清空 和 报错信息错误 -> https://guangdagit.com/beike/bugs/issues/155

--- resources/beike/admin/views/components/form/input-locale.blade.php
+++ resources/beike/admin/views/components/form/input-locale.blade.php
@@ -4,12 +4,12 @@
     <div class="d-flex wp-{{ $width }} input-for-group">
       <span class="input-group-text wp-100 px-1" id="basic-addon1">{{ $locale['name'] }}</span>
       <input type="text" name="{{ $formatName($locale['code']) }}" value="{{ $formatValue($locale['code']) }}"
-        class="form-control short input-{{ $locale['code'] }} {{ $errors->first("descriptions.{$locale['code']}.name") ? 'is-invalid' : '' }}" placeholder="{{ $placeholder ?: $locale['name'] }}" @if ($required) required @endif>
+        class="form-control short input-{{ $locale['code'] }} {{ $errors->first($errorKey($locale['code'])) ? 'is-invalid' : '' }}" placeholder="{{ $placeholder ?: $locale['name'] }}" @if ($required) required @endif>
       @if (!$errors->first("descriptions.{$locale['code']}.name"))
         <span class="invalid-feedback w-auto" style="white-space:nowrap;">{{ __('common.error_required', ['name' => $title]) }}</span>
       @endif
     </div>
-    @if ($errors->first("descriptions.{$locale['code']}.name"))
+    @if ($errors->first($errorKey($locale['code'])))
       <span class="invalid-feedback d-block">{{ $errors->first("descriptions.{$locale['code']}.name") }}</span>
     @endif
     @if ($attributes->has('required'))
@@ -18,7 +18,6 @@
       @enderror
     @endif
   @endforeach
-
   @include('admin::shared.auto-translation')
 </div>
 </x-admin::form.row>
