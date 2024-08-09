5d9ea7de7278f686939f1ececf5fd8a8b41843e8
pushuo <pushuo@guangda.work>
2024-08-09 09:34:52 +0800

    优化 插件列表插件未开启也可以编辑

--- resources/beike/admin/views/pages/plugins/index.blade.php
+++ resources/beike/admin/views/pages/plugins/index.blade.php
@@ -43,10 +43,8 @@
               </td>
               <td>
                 <div v-if="plugin.installed">
-                  <span :style="!plugin.status ? 'cursor: not-allowed':''">
-                    <a v-if="plugin.type != 'theme'" :class="['btn btn-outline-secondary btn-sm', !plugin.status ? 'disabled' : '' ]" :href="plugin.edit_url">{{ __('admin/common.edit') }}</a>
-                    <a v-else :class="['btn btn-outline-secondary btn-sm', !plugin.status ? 'disabled' : '' ]" href="{{ admin_route('theme.index') }}">{{ __('admin/plugin.to_enable') }}</a>
-                  </span>
+                  <a v-if="plugin.type != 'theme'" class="btn btn-outline-secondary btn-sm" :href="plugin.edit_url">{{ __('admin/common.edit') }}</a>
+                  <span v-else :style="!plugin.status ? 'cursor: not-allowed' : ''"><a :class="['btn btn-outline-secondary btn-sm', !plugin.status ? 'disabled' : '' ]"  href="{{ admin_route('theme.index') }}">{{ __('admin/plugin.to_enable') }}</a></span>
                   <a class="btn btn-outline-secondary btn-sm" target="_blank" :href="toBkTicketUrl(plugin.code)">{{ __('admin/plugin.ticket') }}</a>
                   <a class="btn btn-outline-danger btn-sm" @click="installedPlugin(plugin.code, 'uninstall', index)">{{ __('admin/common.uninstall') }}</a>
                 </div>
