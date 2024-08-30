7646892da4424bcc42a3a48c18dd819d33113386
pushuo <pushuo@guangda.work>
2024-08-30 11:42:26 +0800

    修改版本号 -> 1.5.6.2 发布小版本

--- beike/Config/beike.php
+++ beike/Config/beike.php
@@ -11,8 +11,8 @@
 
 return [
     'api_url'         => env('BEIKE_API_URL', 'https://beikeshop.com'),
-    'version'         => '1.5.6.1',
-    'build'           => '20240717',
+    'version'         => '1.5.6.2',
+    'build'           => '20240830',
 
     'admin_name'      => env('ADMIN_NAME'),
     'force_url_https' => env('APP_FORCE_HTTPS', false),
