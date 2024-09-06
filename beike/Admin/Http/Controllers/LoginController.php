f545e1c81101191943f586784a71a57e0c7e5802
撸串青年 <178277164@qq.com>
2024-09-06 20:07:38 +0800

    增加一个hook (#73)
    * Update form.blade.php
    
    * Update LoginController.php
    
    增加一处hook,实现两步验证登录

--- beike/Admin/Http/Controllers/LoginController.php
+++ beike/Admin/Http/Controllers/LoginController.php
@@ -28,6 +28,9 @@ class LoginController extends Controller
 
     public function store(LoginRequest $loginRequest)
     {
+
+        hook_action("admin.login.store.before",$loginRequest);
+        
         if (auth(AdminUser::AUTH_GUARD)->attempt($loginRequest->validated())) {
             return redirect(admin_route('home.index'));
         }
