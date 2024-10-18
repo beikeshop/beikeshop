55da515d55846fe987b5c1d874a07af6f2cf428b
pushuo <pushuo@guangda.work>
2024-10-18 17:13:23 +0800

    优化 上传文件接口大小限制为 10m

--- beike/Shop/Http/Requests/UploadRequest.php
+++ beike/Shop/Http/Requests/UploadRequest.php
@@ -33,7 +33,7 @@ class UploadRequest extends FormRequest
     public function rules()
     {
         return [
-            'file' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:2048',
+            'file' => 'required|image|mimes:jpg,png,jpeg,gif,svg|max:10240',
             'type' => 'required|alpha_dash',
         ];
     }
