2cb944fba8800dc9f346128946bed0ff6afde980
pushuo <pushuo@guangda.work>
2024-11-22 10:41:33 +0800

    优化维护模式 ui +

--- themes/default/maintenance.blade.php
+++ themes/default/maintenance.blade.php
@@ -7,9 +7,9 @@
 
 @section('content')
 <div class="container" id="app">
-  <div class="card mt-5 wp-500 m-auto">
+  <div class="card mt-5 w-max-500 m-auto">
     <div class="">
-      <div class="card-header mt-3 mb-4 px-4 text-center">
+      <div class="card-header mt-3 mb-4 px-lg-4 text-center">
         <div class="mb-5"><img src="{{ asset('image/maintenance-icon.svg') }}" class="img-fluid"></div>
         <h4 class="fw-bold">{{ __('maintenance.maintenance_text') }}</h4>
       </div>
