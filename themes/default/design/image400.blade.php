7d69228d3cba13183f124cfbd1f261a9f8b3d295
pushuo <pushuo@guangda.work>
2024-12-08 18:32:52 +0800

    https://guangdagit.com/beike/bugs/issues/181

--- themes/default/design/image400.blade.php
+++ themes/default/design/image400.blade.php
@@ -5,7 +5,11 @@
     <div class="container-fluid">
       <div class="row g-3 g-lg-4">
         @foreach ($content['images'] as $item)
-        <div class="col-12 col-md-3"><a class="image-wrap" href="{{ $item['link']['link'] ?: 'javascript:void(0)' }}"><img src="{{ $item['image'] }}" class="img-fluid"></a></div>
+        <div class="col-6 col-md-3">
+          <a class="image-wrap d-flex justify-content-center" href="{{ $item['link']['link'] ?: 'javascript:void(0)' }}">
+            <img src="{{ $item['image'] }}" class="img-fluid">
+          </a>
+        </div>
         @endforeach
       </div>
     </div>
