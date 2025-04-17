89d320f94a2ac2cb563c46169ac64a83d82ecdb2
pushuo <pushuo@guangda.work>
2025-04-17 09:49:40 +0800

    chore: update version number 1.6.0.4
    # 优化：更新版本号  1.6.0.4

--- resources/beike/admin/views/pages/plugins/error.blade.php
+++ resources/beike/admin/views/pages/plugins/error.blade.php
@@ -6,12 +6,7 @@
 <div class="card h-min-600">
   <div class="card-body">
     <h6 class="border-bottom pb-3 mb-4">{{ $plugin->getLocaleName() }}</h6>
-    <div class="alert alert-warning py-4 d-flex flex-column align-items-center" role="alert">
-      <div class="mb-2"><i class="bi bi-exclamation-triangle-fill fs-2"></i></div>
-      <div class="mb-4 fs-4">{{ $error }}</div>
-      <a class="btn btn-primary btn-lg" href="{{ admin_route('marketing.show', $plugin_code) }}">{{ __('admin/common.to_buy')
-        }}</a>
-    </div>
+    {!! $error !!}
   </div>
 </div>
 @endsection
