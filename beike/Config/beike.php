a2cda1a99d3d9e87a0987fe33ae01b6a4e713a8e
pushuo <229102104@qq.com>
2025-05-21 16:06:08 +0800

    update build time

--- beike/Config/beike.php
+++ beike/Config/beike.php
@@ -13,7 +13,7 @@ return [
     'api_url'          => env('BEIKE_API', 'https://api.beikeshop.com'),
     'official_website' => env('BEIKE_OFFICIAL_WEBSITE', 'https://beikeshop.com'),
     'version'          => '1.5.6.9',
-    'build'            => '20240925',
+    'build'            => '20250508',
 
     'admin_name'      => env('ADMIN_NAME'),
     'force_url_https' => env('APP_FORCE_HTTPS', false),
